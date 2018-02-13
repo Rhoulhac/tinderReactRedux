7e4db0fd0cbac812bd897b7cf6e35b8892b220ee
Rhoulhac <greg.rhoulhac@gmail.com>
2018-02-13 07:43:40 -0600

    First commit. Also working view to see static booklist

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function BooksReducer() {
+  return [
+    {title: 'The Bible'},
+    {title: 'Dont Waste Your Life'},
+    {title: 'Your Best Life Now'},
+    {title: 'The Gospel According to Jesus'}
+  ];
+}
